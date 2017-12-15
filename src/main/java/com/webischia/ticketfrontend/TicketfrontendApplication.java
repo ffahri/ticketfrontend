fdd1a838839dc3dcb16a5638615d33c7da1619dc
ffahri <admin@webischia.com>
2017-12-15 21:52:33 +0300

    Created Project

---
+++ src/main/java/com/webischia/ticketfrontend/TicketfrontendApplication.java
@@ -0,0 +1,12 @@
+package com.webischia.ticketfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketfrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketfrontendApplication.class, args);
+	}
+}
